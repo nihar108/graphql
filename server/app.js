c3ebb257ea3709fcec140c22f23d82eb4d7c8d7f
Nihar Panigrahi <nihar108@gmail.com>
2018-05-02 18:02:51 +0530

    add models and mutations

--- server/app.js
+++ server/app.js
@@ -2,10 +2,16 @@ const express = require('express');
 
 // allows express to understand graphql. used as a middleware on a single route which will be an endpoint to interact with graphql data.
 const graphqlHTTP = require('express-graphql');
-
 const schema = require('./schema/schema');
 
+// mongoose is an ORM which makes connecting to the db easier
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect('mongodb://nihar:password@ds159509.mlab.com:59509/gql-db')
+mongoose.connection.once('open', () => {
+  console.log('connected to mlab');
+});
 
 // /graphql route that we use the middleware on
 // it needs a schema to know what the graph exactly looks
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -8,37 +8,45 @@ const {
   GraphQLString,
   GraphQLSchema,
   GraphQLID,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull,
 } = graphql;
 
-// dummy data
-var books = [
-  { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
-  { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
-  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
-];
+const Book = require('../models/book');
+const Author = require('../models/author');
 
-const authors = [
-  { name: 'Patrick Rothfuss', age: 44, id: '1' },
-  { name: 'Brandon Sanderson', age: 42, id: '2' },
-  { name: 'Terry Pratchett', age: 66, id: '3' }
-];
+// dummy data
+// var books = [
+//   { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+//   { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+//   { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+//   { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+//   { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '2' },
+//   { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
+// ];
+
+// const authors = [
+//   { name: 'Patrick Rothfuss', age: 44, id: '1' },
+//   { name: 'Brandon Sanderson', age: 42, id: '2' },
+//   { name: 'Terry Pratchett', age: 66, id: '3' }
+// ];
 
 // define object types
 const BookType = new GraphQLObjectType({
   name: 'Book',
-  // things we send back to the client
+  // things we CAN send back to the client
   fields: () => ({
     id: { type: GraphQLID }, // GraphQLID adds flexibility, graphql converts the arg to string internally
-    name: { type: GraphQLString },
+    name: {
+      type: GraphQLString 
+    },
     genre: { type: GraphQLString },
     author: {
       type: AuthorType,
       // parent has book result that we get from the query
       resolve(parent, args) {
-        return authors.filter(author => {
-          return parent.authorId === author.id;
-        })[0]
+        return Author.findById(parent.authorId)
       }
     }
   })
@@ -50,6 +58,12 @@ const AuthorType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return Book.find({ authorId: parent.id})
+      } 
+    }
   })
 });
 
@@ -63,25 +77,76 @@ const RootQuery = new GraphQLObjectType({
       resolve(parent, args) {
         // fired when we receive the query
         // code to get data form db / other sources
-        console.log(typeof(args.id));
-        return books.filter(book => {
-          return book.id === args.id
-        })[0];
+        return Book.findById(args.id);
       }
     },
     author: {
       type: AuthorType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return authors.filter(author => {
-          return author.id === args.id
-        })[0];
+        return Author.findById(args.id);
+      }
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return Book.find({});
+      }
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve(parent, args) {
+        return Author.find({});
       }
     }
   }
 });
 
-module.exports = new GraphQLSchema({
-  query: RootQuery
+const Mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addAuthor: {
+      type: AuthorType,
+      args: {
+        name: {type: GraphQLString},
+        age: {type: GraphQLInt}
+      },
+      resolve(parent, args) {
+        let author = new Author({
+          name: args.name,
+          age: args.age,
+        });
+
+        /*
+        technically you can return any model from here (eg. books)
+        as long as the queried fields are present in the "type" of the this mutation
+        
+        mongoose returns the same object after saving
+        */
+        return author.save();
+      }
+    },
+    addBook: {
+      type: BookType,
+      args: {
+        name: { type: new GraphQLNonNull(GraphQLString)},
+        genre: { type: new GraphQLNonNull(GraphQLString)},
+        authorId: { type: new GraphQLNonNull(GraphQLID)}
+      },
+      resolve(parent, args) {
+        let book = new Book({
+          name: args.name,
+          genre: args.genre,
+          authorId: args.authorId
+        });
+
+        return book.save();
+      }
+    }
+  }
 })
 
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+  mutation: Mutation,
+})
